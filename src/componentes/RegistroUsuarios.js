1cf624f12e96fa9ec62b3f93e11ca25b91107811
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-03-16 15:07:38 -0600

    Se crea el formulario para Crear una cuenta, con su Header, inputs para correo, password y passwordConfirm y el boton para crear la cuenta

--- src/componentes/RegistroUsuarios.js
+++ src/componentes/RegistroUsuarios.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {Helmet} from 'react-helmet';
+import styled from 'styled-components';
+
+import {Header, Titulo, ContenedorHeader, ContenedorBotones} from '../elementos/Header';
+import {Formulario, Input, ContenedorBoton} from '../elementos/ElementosDeFormulario';
+import Boton from '../elementos/Boton';
+import {ReactComponent as SvgLogin} from '../images/registro.svg';
+
+const Svg = styled(SvgLogin)`
+  width: 100%;
+  max-height: 6.25rem; /* 100px */
+  margin-bottom: 1.25rem; /* 20px */
+`;
 
 const RegistroUsuarios = () => {
   return ( 
-    <h1>Registo Usuarios</h1>
+    <Fragment>
+
+      <Helmet>
+        <title>Crear Cuenta</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Crear Cuenta</Titulo>
+          <div>
+            <Boton to="/iniciar-sesion">Iniciar Sesión</Boton>
+          </div>
+        </ContenedorHeader>      
+      </Header>
+
+      <Formulario>
+        <Svg />
+        <Input 
+          type="email"
+          name="email"
+          placeholder="Correo Electrónico"
+        />
+        <Input 
+          type="passwo rd"
+          name="password"
+          placeholder="Contraseña"
+        />
+        <Input 
+          type="password"
+          name="passwordConfirm"
+          placeholder="Repetir Contraseña"
+        />
+        <ContenedorBoton>
+          <Boton as="button" primario type="submit">Crear Cuenta</Boton>        
+        </ContenedorBoton>
+      </Formulario>
+
+    </Fragment>
   );
 }
  
