bbb699ec5447474afe1166edc1e0ef91e70a24f7
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-03-16 12:15:02 -0600

    Se crean los componentes principales de la aplicación

---
+++ src/componentes/GastosPorCategoria.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GastosPorCategoria = () => {
+  return ( 
+    <h1>Gastos por Categoria</h1>
+  );
+}
+ 
+export default GastosPorCategoria;
---
+++ src/componentes/ListaDeGastos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ListaDeGastos = () => {
+  return ( 
+    <h1>Lista de Gastos</h1>
+  );
+}
+ 
+export default ListaDeGastos;
