642a014a1773936ebe994b27fd1b59bec7c4144c
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-03-16 12:17:44 -0600

    Se configura el Router en el Contenedor principal, esto se hace en el index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WebFont from 'webfontloader';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+
+// de style component
+import Contenedor from './elementos/Contenedor';
+
+// Componentes
+import EditarGasto from './componentes/EditarGasto';
+import GastosPorCategoria from './componentes/GastosPorCategoria';
+import InicioSesion from './componentes/InicioSesion';
+import ListaDeGastos from './componentes/ListaDeGastos';
+import RegistroUsuarios from './componentes/RegistroUsuarios';
 
 // se habilita el web font loader
 WebFont.load({
@@ -14,7 +25,18 @@ WebFont.load({
 
 const Index = () => {
 	return (
-		<App />
+		<BrowserRouter>
+			<Contenedor>
+				<Switch>
+					<Route path="/iniciar-sesion" component={InicioSesion} />
+					<Route path="/crear-cuenta" component={RegistroUsuarios} />
+					<Route path="/categorias" component={GastosPorCategoria} />
+					<Route path="/lista" component={ListaDeGastos} />
+					<Route path="/editar/:id" component={EditarGasto} />
+					<Route path="/" component={App} />
+				</Switch>
+			</Contenedor>		
+		</BrowserRouter>
 	);
 }
 
