1a3a8c1598c4dfbe057a1072ef32f18db4eaa7e0
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-03-17 13:28:26 -0600

    Se valida el registro de usuarios: que sea un correo valido(con expresión regular), que no se envien campos vacios y que los passwords sean iguales

--- src/componentes/RegistroUsuarios.js
+++ src/componentes/RegistroUsuarios.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Helmet} from 'react-helmet';
 import styled from 'styled-components';
 
@@ -14,6 +14,66 @@ const Svg = styled(SvgLogin)`
 `;
 
 const RegistroUsuarios = () => {
+
+  // se define los state para cada input
+  const [correo, establecerCorreo] = useState('');
+  const [password, establecerPassword] = useState('');
+  const [passwordConfirm, establecerPasswordConfirm] = useState('');
+
+  // FUNCIONES
+  const handleChangeInputs = e => {
+    // console.log(e.target.name);
+    switch (e.target.name) {
+      case 'email':
+          establecerCorreo(e.target.value);
+      break;
+      case 'password':
+          establecerPassword(e.target.value);
+      break;
+      case 'passwordConfirm':
+          establecerPasswordConfirm(e.target.value);
+      break;
+      default:
+        break
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    /* TODO:
+          validar que sea un correo
+          validar que ese correo no este registrado
+          validar que password y passwordConfirm sean iguales
+          validar alguna logitud minima del password
+    */
+
+    // Comprobar que el correo sea valido por medio de una expresión reguñlar
+    const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+    // console.log(expresionRegular.test(correo));
+    if( !expresionRegular.test(correo) ){
+      // si no hay un correo valido
+      console.log('Por favor ingresa un correo electronico valido');
+      return;
+    }
+
+    // Se valida que ningun campo se quede vacio
+    if( correo === '' || password === '' || passwordConfirm ==='' ){
+      console.log('Todos los datos son obligatorios');
+      return;
+    }
+
+    // validamos que el password y passwordConfirm sean iguales
+    if ( password !== passwordConfirm) {
+      console.log('Las contraseñas deben ser iguales');
+      return;
+    }
+
+    // Quiere decir que se han pasado todas las validaciones.
+    console.log('Registramos usuario');
+
+  }
+
   return ( 
     <Fragment>
 
@@ -30,22 +90,30 @@ const RegistroUsuarios = () => {
         </ContenedorHeader>      
       </Header>
 
-      <Formulario>
+      <Formulario
+        onSubmit={handleSubmit}
+      >
         <Svg />
         <Input 
           type="email"
           name="email"
           placeholder="Correo Electrónico"
+          value={correo}
+          onChange={handleChangeInputs}
         />
         <Input 
-          type="passwo rd"
+          type="password"
           name="password"
           placeholder="Contraseña"
+          value={password}
+          onChange={handleChangeInputs}
         />
         <Input 
           type="password"
           name="passwordConfirm"
           placeholder="Repetir Contraseña"
+          value={passwordConfirm}
+          onChange={handleChangeInputs}
         />
         <ContenedorBoton>
           <Boton as="button" primario type="submit">Crear Cuenta</Boton>        
